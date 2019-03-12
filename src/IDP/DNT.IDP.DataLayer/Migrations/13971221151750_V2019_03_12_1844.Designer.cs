d4e09ec07a87f477d79960ee44d4d13616786912
VahidN <vahid_nasiri@yahoo.com>
2019-03-12 18:48:48 +0330

    Added migrations

--- src/IDP/DNT.IDP.DataLayer/Migrations/13971221151750_V2019_03_12_1844.Designer.cs
+++ src/IDP/DNT.IDP.DataLayer/Migrations/13971221151750_V2019_03_12_1844.Designer.cs
@@ -4,14 +4,16 @@ using DNT.IDP.DataLayer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DNT.IDP.DataLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("13971221151750_V2019_03_12_1844")]
+    partial class V2019_03_12_1844
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
